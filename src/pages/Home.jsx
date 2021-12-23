e87a360188dad2eaf1de363ed75e238e4c64c9ea
Kirill <mustang.horse34@gmail.com>
2021-12-23 11:14:33 +0700

    Added react query

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,65 +5,80 @@ import Grid from '@mui/material/Grid';
 import { Post } from '../components/index.js';
 import { TagsBlock } from '../components/index.js';
 import { CommentsBlock } from '../components/index.js';
+import { useQuery } from 'react-query';
+import { Alert } from '@mui/material';
 
 function Home() {
+  async function getPosts() {
+    const res = await fetch('http://localhost:4000/posts');
+    const data = await res.json();
+    return data;
+  }
+  const { data, isLoading, isError } = useQuery('posts', getPosts);
+
   return (
     <>
-      <Tabs
-        style={{ marginBottom: 15 }}
-        value={0}
-        aria-label='basic tabs example'
-      >
-        <Tab label='Новые' />
-        <Tab label='Популярные' />
-      </Tabs>
-      <Grid container spacing={4}>
-        <Grid xs={8} item>
-          {[...Array(5)].map((item, index) => (
-            <Post
-              key={index}
-              id={1}
-              title='Roast the code #1 | Rock Paper Scissors'
-              imageUrl='https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png'
-              user={{
-                avatarUrl:
-                  'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                fullName: 'Keff',
-              }}
-              createdAt={'12 июня 2022 г.'}
-              viewsCount={150}
-              commentsCount={3}
-              tags={['react', 'fun', 'typescript']}
-              isEditable
-            />
-          ))}
-        </Grid>
-        <Grid xs={4} item>
-          <TagsBlock
-            items={['react', 'typescript', 'заметки']}
-            isLoading={false}
-          />
-          <CommentsBlock
-            items={[
-              {
-                user: {
-                  fullName: 'Кирилл Горшков',
-                  avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
-                },
-                text: 'Это тестовый комментарий',
-              },
-              {
-                user: {
-                  fullName: 'Иван Бочковский',
-                  avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
-                },
-                text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
-              },
-            ]}
-            isLoading={false}
-          />
-        </Grid>
-      </Grid>
+      {isError ? (
+        <Alert severity='error'>Ошибка соединения с сервером</Alert>
+      ) : (
+        <>
+          <Tabs
+            style={{ marginBottom: 15 }}
+            value={0}
+            aria-label='basic tabs example'
+          >
+            <Tab label='Новые' />
+            <Tab label='Популярные' />
+          </Tabs>
+          <Grid container spacing={4}>
+            <Grid xs={8} item>
+              {(isLoading ? [...Array(5)] : data).map((item, index) =>
+                isLoading ? (
+                  <Post key={index} isLoading={true} />
+                ) : (
+                  <Post
+                    key={item._id}
+                    id={item._id}
+                    title={item.title}
+                    imageUrl='https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png'
+                    user={item.author}
+                    createdAt={item.createdAt}
+                    viewsCount={item.viewCount}
+                    commentsCount={3}
+                    tags={item.tags}
+                    isEditable
+                  />
+                )
+              )}
+            </Grid>
+            <Grid xs={4} item>
+              <TagsBlock
+                items={['react', 'typescript', 'заметки']}
+                isLoading={false}
+              />
+              <CommentsBlock
+                items={[
+                  {
+                    user: {
+                      fullName: 'Кирилл Горшков',
+                      avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+                    },
+                    text: 'Это тестовый комментарий',
+                  },
+                  {
+                    user: {
+                      fullName: 'Иван Бочковский',
+                      avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
+                    },
+                    text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
+                  },
+                ]}
+                isLoading={false}
+              />
+            </Grid>
+          </Grid>
+        </>
+      )}
     </>
   );
 }
