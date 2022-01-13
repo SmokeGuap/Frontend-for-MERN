eab50770a209535c865fbc1367884ce2111e52c5
Kirill <mustang.horse34@gmail.com>
2022-01-13 10:15:21 +0700

    Added Login logic

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,24 +4,63 @@ import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 
 import styles from './Login.module.scss';
+import { useForm } from 'react-hook-form';
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+  async function login(data) {
+    const res = await fetch('http://localhost:4000/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const user = await res.json();
+    console.log(user);
+    return user;
+  }
+  const onSumbit = (data) => {
+    login(data);
+  };
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant='h5'>
         Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label='E-Mail'
-        error
-        helperText='Неверно указана почта'
-        fullWidth
-      />
-      <TextField className={styles.field} label='Пароль' fullWidth />
-      <Button size='large' variant='contained' fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSumbit)}>
+        <TextField
+          className={styles.field}
+          label='E-Mail'
+          type='email'
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          {...register('email', { required: 'Укажите почту' })}
+          fullWidth
+        />
+        <TextField
+          type='password'
+          className={styles.field}
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register('password', { required: 'Укажите пароль' })}
+          label='Пароль'
+          fullWidth
+        />
+        <Button type='submit' size='large' variant='contained' fullWidth>
+          Войти
+        </Button>
+      </form>
     </Paper>
   );
 }
