cbe3dc03f5725efad71034837fdf0798024235a2
Kirill <mustang.horse34@gmail.com>
2023-04-26 16:28:13 +0700

    AddPost pages

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,17 @@
 import Header from './Header/Header.jsx';
 import AddComment from './AddComment/AddComment.jsx';
 import Post from './Post/Post.jsx';
+import SideBlock from './SideBlock/SideBlock.jsx';
+import UserInfo from './UserInfo/UserInfo.jsx';
+import CommentsBlock from './CommentsBlock.jsx';
+import TagsBlock from './TagsBlock.jsx';
 
-export { Header, AddComment, Post };
+export {
+  Header,
+  AddComment,
+  Post,
+  SideBlock,
+  UserInfo,
+  CommentsBlock,
+  TagsBlock,
+};
