a8a549d7a743e580cff8218947698fe93fea90d8
Kirill <mustang.horse34@gmail.com>
2023-04-26 16:31:13 +0700

    Added Login and Registration pages

--- src/pages/AddPost/AddPost.jsx
+++ src/pages/AddPost/AddPost.jsx
@@ -1,10 +1,10 @@
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
-import SimpleMDE from 'react-simplemde-editor';
 
-import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
+import SimpleMDE from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
 
 export const AddPost = () => {
   const imageUrl = '';
@@ -30,39 +30,53 @@ export const AddPost = () => {
         delay: 1000,
       },
     }),
-    [],
+    []
   );
 
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button variant='outlined' size='large'>
         Загрузить превью
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input type='file' onChange={handleChangeFile} hidden />
       {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+        <Button variant='contained' color='error' onClick={onClickRemoveImage}>
           Удалить
         </Button>
       )}
       {imageUrl && (
-        <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        <img
+          className={styles.image}
+          src={`http://localhost:4444${imageUrl}`}
+          alt='Uploaded'
+        />
       )}
       <br />
       <br />
       <TextField
         classes={{ root: styles.title }}
-        variant="standard"
-        placeholder="Заголовок статьи..."
+        variant='standard'
+        placeholder='Заголовок статьи...'
         fullWidth
       />
-      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth />
-      <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
+      <TextField
+        classes={{ root: styles.tags }}
+        variant='standard'
+        placeholder='Тэги'
+        fullWidth
+      />
+      <SimpleMDE
+        className={styles.editor}
+        value={value}
+        onChange={onChange}
+        options={options}
+      />
       <div className={styles.buttons}>
-        <Button size="large" variant="contained">
+        <Button size='large' variant='contained'>
           Опубликовать
         </Button>
-        <a href="/">
-          <Button size="large">Отмена</Button>
+        <a href='/'>
+          <Button size='large'>Отмена</Button>
         </a>
       </div>
     </Paper>
