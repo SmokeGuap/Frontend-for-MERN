a1feabeb20d335195f682b7d01c1f7325c04fb13
Kirill <mustang.horse34@gmail.com>
2023-04-26 16:40:50 +0700

    Some refactoring

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home.jsx';
+import FullPost from './FullPost.jsx';
+import AddPost from './AddPost/AddPost.jsx';
+import Registration from './Registration/Registration.jsx';
+import Login from './Login/Login.jsx';
+
+export { Home, FullPost, AddPost, Registration, Login };
