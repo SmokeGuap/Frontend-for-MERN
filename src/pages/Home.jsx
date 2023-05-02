11c9183374ee64b04055856f4be1649f21f57233
Kirill <mustang.horse34@gmail.com>
2023-05-03 01:41:41 +0700

    Added Login logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,7 +54,7 @@ function Home() {
                     key={item._id}
                     id={item._id}
                     title={item.title}
-                    imageUrl='https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png'
+                    imageUrl={item.imageUrl}
                     user={item.author}
                     createdAt={item.createdAt}
                     viewsCount={item.viewCount}
